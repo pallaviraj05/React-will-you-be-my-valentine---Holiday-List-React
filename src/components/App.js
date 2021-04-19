209a0551dd27f3346eda5618346863bbff9f8ef1
pallaviraj05 <pallavi05031998@gmail.com>
2021-04-19 15:40:55 +0530

    holidaylist_react

--- src/components/App.js
+++ src/components/App.js
@@ -39,9 +39,19 @@ class App extends Component {
 
   render() {
     return (
-      <div id="main">
-        {/* Do not remove the main div */}
-      </div>
+      
+        
+
+        <div id="main">
+               {/* Do not remove the main div */}
+        <ol key={"relativeList"} >
+            <li key={"relativeListItem1"}>Sunil Salunkhe</li>
+            <li key={"relativeListItem2"}>Kisan Sathe</li>
+            <li key={"relativeListItem3"}>Kundalik Bhosale</li>
+            <li key={"relativeListItem4"}>Moahn Patil</li>
+        </ol>
+            </div>
+     
     )
   }
 }
